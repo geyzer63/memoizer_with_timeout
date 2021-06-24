bb87240186464973bb43e5c5c8d49f9c97d0e622
geyzer63 <mark.geyzer@gmail.com>
2021-06-24 09:45:36 +0300

    Update memoizer_with_timeout.py
    Improved cache management, The original code was refactored from a function decorator, and did not provide good separation between instance data and class data.

--- memoizer_with_timeout.py
+++ memoizer_with_timeout.py
@@ -4,59 +4,66 @@ from threading import Lock
 
 class TimedMemoizer:
     """ 
-    Memoization decorator, caching function call results till 
-    call time for the combination of function and argument values is 
-    greater than cached expiration timestamp.
+    Memoization decorator, caching function call results
+    Call results expire cache expires when call time for a function with 
+    cached argument values is  greater than cached expiration timestamp.
     On a call to any decorated function, cache audit may be invoked
     in order to clear stale cached results and prevent potential memory leak.
     """
     __CACHE_AUDIT_TO = 1800
-    __cache = {}
-    __next_cleanup = time()
+    __instances = []
+    __closest_audit_ts = time() + __CACHE_AUDIT_TO
     
     @classmethod
-    def __stale_cache_clear(cls, now):
-        """ Executes clean-up of stale cached calls """
-        stale_results = []
-        for func, func_cache in cls.__cache.items():
-            for result_key, (_, expires_at) in func_cache.items():
-                if expires_at < now:
-                    stale_results.append((func, result_key))
-                        
-        for func, result_key in stale_results:
-            cls.__cache[func].pop(result_key)
-            if not cls.__cache[func]:
-                cls.__cache.pop(func)
-        cls.__next_cleanup = now + cls.__CACHE_AUDIT_TO
-   
+    def __run_stale_caches_audit(cls, now):
+        """ Executes clean-up of stale cached calls over all instances """
+        if cls.__closest_audit_ts < now:
+            cls.__closest_audit_ts = time() + cls.__CACHE_AUDIT_TO
+            with Lock():
+                for instance in cls.__instances:
+                    instance.__clear_stale_results(now)
+    
     def __init__(self, timeout):
-        self.timeout = timeout
+        self.__cache = {}
+        self.__timeout = timeout
+        self.__next_audit_ts = 0
+        self.__class__.__instances.append(self)
+        self.__set_next_audit(time())
     
-    @classmethod
-    def __clear(cls, func):
-        """ Allows clearing of all cached results for a specific function """
-        cls.__cache.pop(func)
+    def __set_next_audit(self, now):
+        self.__next_audit_ts = max(now + self.__timeout,
+                                   self.__class__.__closest_audit_ts)
+    
+    def __clear(self, func):
+        """ Allows clearing of all cached results """
+        self.__cache.clear()
+    
+    def __clear_stale_results(self, now):
+        """ CLean-up stale function cache """
+        if self.__next_audit_ts < now:
+            self.__cache = {
+                result_key: (result, expires_at)
+                for result_key, (result, expires_at) in self.__cache.items()
+                if expires_at > now
+            }
+            self.__set_next_audit(now)
         
     def __call__(self, func):
-        clazz = self.__class__
-        setattr(func, 'clear', partial(clazz.__clear, func))
+        setattr(func, 'clear', self.__clear)
         @wraps(func)
         def caller(*args, **kwargs):
             now = int(time())
-            if now > clazz.__next_cleanup:
-                # Execute next cache audit
-                with Lock():
-                    clazz.__stale_cache_clear(now)
+            self.__class__.__run_stale_caches_audit(now)
 
-            func_cache = clazz.__cache.setdefault(func, {})
             result_key = hash(f'{args}{kwargs}')
-            result, expires_at = func_cache.get(result_key, (None, 0))
+            result, expires_at = self.__cache.get(result_key, (None, 0))
             if expires_at > now:
                 return result
             
-            expires_at = now + self.timeout
+            expires_at = now + self.__timeout
             result = func(*args, **kwargs)
-            func_cache[result_key] = result, expires_at         
+            self.__cache[result_key] = result, expires_at
             return result
             
         return caller
+
